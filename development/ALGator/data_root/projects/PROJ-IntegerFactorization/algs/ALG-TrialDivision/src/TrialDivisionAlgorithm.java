84aceb7109a1bdd37774488bf3d4e5c6b2fbcd37
Annch <annch.borovac@gmail.com>
2018-06-24 16:33:52 +0200

    Nekoliko sprememnjen TrialDivisionAlgorithm

--- development/ALGator/data_root/projects/PROJ-IntegerFactorization/algs/ALG-TrialDivision/src/TrialDivisionAlgorithm.java
+++ development/ALGator/data_root/projects/PROJ-IntegerFactorization/algs/ALG-TrialDivision/src/TrialDivisionAlgorithm.java
@@ -12,32 +12,23 @@ public class TrialDivisionAlgorithm extends IntegerFactorizationAbsAlgorithm {
 	static BigInteger zero = new BigInteger("0");
 	static BigInteger one = new BigInteger("1");
 	static BigInteger two = new BigInteger("2");
-	static BigInteger three = new BigInteger("3");
 	
 	@Override
   public ArrayList<BigInteger> execute(BigInteger number) {
 		// http://eprints.fri.uni-lj.si/1356/2/Bogataj1.pdf
 		
 		ArrayList<BigInteger> factors = new ArrayList<BigInteger>();
-		BigInteger n = number;
-		
-		while ( n.mod(two).compareTo(zero) == 0 ) {
-			n = n.divide(two);
-			factors.add(two);
-			factors.add(n);
-			return factors;
-		}
-		
-		BigInteger d = three;
+		BigInteger n = number;		
+		BigInteger d = two;
 		
 		while ( d.multiply(d).compareTo(n) != 1 ) {
-			while ( n.mod(d).equals(zero) ) {
+			if ( n.mod(d).equals(zero) ) {
 				n = n.divide(d);
 				factors.add(d);
 				factors.add(n);
 				return factors;
 			}
-			d = d.add(two);
+			d = d.add(one);
 		}
 		
 		if ( n.equals(one) ) {
